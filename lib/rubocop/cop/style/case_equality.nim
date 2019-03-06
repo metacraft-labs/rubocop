ba905e7c810b067c3092b80f1ffc26b933803d0b
Alexander Ivanov <alehander42@gmail.com>
2019-03-07 01:39:23 +0200

    New block macro

--- lib/rubocop/cop/style/case_equality.nim
+++ lib/rubocop/cop/style/case_equality.nim
@@ -1,17 +1,13 @@
 
+import
+  types
+
 type
   CaseEquality* = ref object of Cop
-    options*: Hash
-    offenses*: Array
-    processedSource*: NilClass
-    corrections*: Array
-    correctedNodes*: Hash
-    config*: Config
-
 const
   MSG = "Avoid the use of the case equality operator `===`."
 nodeMatcher isCaseEquality, "(send _ :=== _)"
 proc onSend*(self: CaseEquality; node: Node): void =
-  isCaseEquality(node, proc (): void =
-    addOffense(node, location = "selector"))
+  isCaseEquality node:
+    addOffense(node, location = "selector")
 
--- spec/rubocop/cop/style/case_equality_spec.nim
+++ spec/rubocop/cop/style/case_equality_spec.nim
@@ -1,5 +1,8 @@
 
-suite "CaseEquality",
+import
+  types
+
+suite "CaseEquality":
   var cop = initClassVars()
   it("registers an offense for ===", proc (): void =
     expectOffense("(str \"      Array === var\\n\")".stripIndent))
