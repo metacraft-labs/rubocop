b65a104a6edfbb288c411b43b3d0e51a62318aa7
Alexander Ivanov <alehander42@gmail.com>
2019-03-07 17:30:52 +0200

    New

--- lib/rubocop/cop/style/case_equality.nim
+++ lib/rubocop/cop/style/case_equality.nim
@@ -5,10 +5,10 @@ import
 cop :
   type
     CaseEquality* = ref object of Cop
+  const
+    MSG = "Avoid the use of the case equality operator `===`."
+  nodeMatcher isCaseEquality, "(send _ :=== _)"
   method onSend*(self: CaseEquality; node: Node): void =
     isCaseEquality node:
       addOffense(node, location = "selector")
 
-  const
-    MSG = "Avoid the use of the case equality operator `===`."
-  nodeMatcher isCaseEquality, "(send _ :=== _)"
