b008af45ebaa2ab33ec1411a132c84960079415f
Alexander Ivanov <alehander42@gmail.com>
2019-05-03 09:04:55 +0300

    Fix

--- spec.sh
+++ spec.sh
@@ -1,5 +1,7 @@
 cd ~/rubocop
-rbenv local 2.5.0
+# rbenv local 2.5.0
 
-rbenv exec bundle exec ruby /home/alehander42/.rbenv/versions/2.5.0/bin/rspec ~/rubocop/spec/rubocop/cop/$2/$1_spec.rb
+# rbenv exec bundle exec ruby /home/alehander42/.rbenv/versions/2.5.0/bin/rspec ~/rubocop/spec/rubocop/cop/$2/$1_spec.rb
+# cd ~/rubocop
+bundle exec rspec ~/rubocop/spec/rubocop/cop/$2/$1_spec.rb
 
